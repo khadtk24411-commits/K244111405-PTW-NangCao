53ee6a3bca29a7aa1b3ddb72d386a224331f429c
Diep Tuy Kha <khadtk24411@st.uel.edu.vn>
2026-09-24 22:28:45 +0700

    Update app-module.ts

--- LEARNANGULAR/app/src/app/app-module.ts
+++ LEARNANGULAR/app/src/app/app-module.ts
@@ -6,11 +6,13 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 import { App } from './app';
 import { AppRoutingModule } from './app-routing-module';
+
 import { Categorizeproduct } from './categorizeproduct/categorizeproduct';
 import { ServiceProductImageEvent } from './service-product-image-event/service-product-image-event';
 import { ServiceProductImageEventDetail } from './service-product-image-event-detail/service-product-image-event-detail';
 import { CustomerListComponent } from './customer-list-component/customer-list-component';
 import { Homework } from './homework/homework';
+import { Customerlistcallhttpservice } from './customerlistcallhttpservice/customerlistcallhttpservice';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { Homework } from './homework/homework';
     ServiceProductImageEvent,
     ServiceProductImageEventDetail,
     CustomerListComponent,
-    Homework
+    Homework,
+    Customerlistcallhttpservice
   ],
 
   imports: [
